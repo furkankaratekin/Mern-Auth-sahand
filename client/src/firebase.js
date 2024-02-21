864ad4b8a1d4a2cbe8ad2e1b90b5f558cbeeaeda
furkankaratekin <furkankaratekin.2k1@gmail.com>
2024-02-21 14:45:01 +0300

    bitirme icin

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB2Xp2qHpiufIUoxOWDeCXtIdsBSisQtQw",
+  authDomain: "mern-auth-21-02-2024-f31af.firebaseapp.com",
+  projectId: "mern-auth-21-02-2024-f31af",
+  storageBucket: "mern-auth-21-02-2024-f31af.appspot.com",
+  messagingSenderId: "134251373024",
+  appId: "1:134251373024:web:2b080d43ec96819721cb85",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
